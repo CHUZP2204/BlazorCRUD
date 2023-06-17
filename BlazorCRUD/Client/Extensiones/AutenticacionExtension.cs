88312e6755c6df6a6706662314be16c3be1e4853
Jesús Pérez <jesussaenz04@gmail.com>
2023-06-16 21:54:00 -0600

    Autenticacion Clases, Configuracion De Servicios en App.razor, Configuracion de Program.cs

--- BlazorCRUD/Client/Extensiones/AutenticacionExtension.cs
+++ BlazorCRUD/Client/Extensiones/AutenticacionExtension.cs
@@ -1,6 +1,60 @@
-﻿namespace BlazorCRUD.Client.Extensiones
+﻿using Blazored.SessionStorage;
+using BlazorCRUD.Shared;
+
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.AccessControl;
+using System.Security.Claims;
+
+namespace BlazorCRUD.Client.Extensiones
 {
-    public class AutenticacionExtension
+    public class AutenticacionExtension : AuthenticationStateProvider
     {
+
+        private readonly ISessionStorageService _sessionStorage;
+        private ClaimsPrincipal _sinInformacion = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public AutenticacionExtension(ISessionStorageService sessionStorage)
+        {
+            _sessionStorage = sessionStorage;
+        }
+
+        public async Task ActualizarEstadoAutenticacion(Usuario? sesionUsuario)
+        {
+            ClaimsPrincipal claimsPrincipal;
+            if (sesionUsuario != null)
+            {
+                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
+                    new Claim(ClaimTypes.Email,sesionUsuario.Email)
+                }, "JwtAuth"));
+
+                await _sessionStorage.GuardarStorage("sesionUsuario", sesionUsuario);
+            }
+            else
+            {
+                claimsPrincipal = _sinInformacion;
+                await _sessionStorage.RemoveItemAsync("sesionUsuario");
+            }
+
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var sesionUsuario = await _sessionStorage.ObtenerStorage<Usuario>("sesionUsuario");
+
+            if (sesionUsuario == null)
+                return await Task.FromResult(new AuthenticationState(_sinInformacion));
+
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+           {
+               new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
+                    new Claim(ClaimTypes.Email,sesionUsuario.Email)
+           }, "JwtAuth"));
+
+            return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+        }
     }
 }
--- BlazorCRUD/Client/Program.cs
+++ BlazorCRUD/Client/Program.cs
@@ -1,12 +1,24 @@
 using BlazorCRUD.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+
+///Enlaces para el login
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using BlazorCRUD.Client.Extensiones;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+//Servicios para el login
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, AutenticacionExtension>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
 
