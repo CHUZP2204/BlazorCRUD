4d6353440eeaffb4c9323af3539e10f5b4f6fdb6
Jesús Pérez <jesussaenz04@gmail.com>
2023-10-03 21:56:02 -0600

    Prueba DBContext

--- BlazorCRUD/Server/Modelos/PruebablazorContext.cs
+++ BlazorCRUD/Server/Modelos/PruebablazorContext.cs
@@ -22,9 +22,6 @@ public partial class PruebablazorContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-MQE65RA; Initial Catalog=Pruebablazor; Integrated Security=True; Connect Timeout=30; Encrypt=False; TrustServerCertificate=False; ApplicationIntent=ReadWrite; multiSubnetFailOver=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
